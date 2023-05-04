67e9f7529c7a0fa42d0720dc6a477e07f9db9a37
sam2much96 <sam2much96@gmail.com>
2023-05-04 17:25:35 +0100

    modified:   videoscript.py 	versioning.py

--- videoscript.py
+++ videoscript.py
@@ -5,7 +5,7 @@ import pyttsx3  # for voiceovers
 import os
 import subprocess
 import ffmpeg
-#from mutagen.mp3 import MP3
+# from mutagen.mp3 import MP3
 import time
 MAX_WORDS_PER_COMMENT = 100
 MIN_COMMENTS_FOR_FINISH = 4
@@ -14,13 +14,13 @@ MAX_DURATION = 58
 
 
 class VideoScript:
-    title : str = ""
-    fileName : str = ""
+    title: str = ""
+    fileName: str = ""
     titleSCFile = ""
-    titleAudioDuration : int = 0
+    titleAudioDuration: int = 0
     url = ""
     totalDuration = 0
-   
+
     # Holds frame data
     frames = []
 
@@ -28,11 +28,11 @@ class VideoScript:
     frame = []
 
     file_path = ""
-    
+
     titleAudioClip = AudioFileClip
     audioClip = AudioFileClip
 
-    bit_rate : int = 0
+    bit_rate: int = 0
 
     def __init__(self, url, title, fileId, file_path, audioClip):
         self.fileName = f"{datetime.today().strftime('%Y-%m-%d')}-{fileId}"
@@ -49,18 +49,21 @@ class VideoScript:
         self.frames = []
         self.frame = []
 
-
     def canBeFinished(self) -> bool:
-        return_value_2 : bool = (len(self.frames) > 0) and (self.totalDuration > MIN_DURATION)
-        print(f"can Be Finished : {return_value_2} , min duration: {self.totalDuration }")  # for debug purposes only
+        return_value_2: bool = (len(self.frames) > 0) and (
+            self.totalDuration > MIN_DURATION)
+        # for debug purposes only
+        print(
+            f"can Be Finished : {return_value_2} , min duration: {self.totalDuration }")
         return return_value_2
 
     def canQuickFinish(self) -> bool:
         return_value: bool = (len(self.frames) >= MIN_COMMENTS_FOR_FINISH) and (
             self.totalDuration > MIN_DURATION)
-        print(f"can Quick finish debug : {return_value}")  # for debug purposes only
+        # for debug purposes only
+        print(f"can Quick finish debug : {return_value}")
         return return_value
-        #return True
+        # return True
 
     def addCommentScene(self, text, commentId) -> bool:
         # Get the word count
@@ -85,7 +88,7 @@ class VideoScript:
 
         # if (frame.audioClip == None):
         #    return True
-        #self.frames.append(frame)
+        # self.frames.append(frame)
         return True
 
     def getDuration(self):
@@ -100,7 +103,6 @@ class VideoScript:
         audioClip = VoiceOver.create_voice_over(
             VoiceOver, f"{self.fileName}-{name}", text)
 
-
         # Error checker 2
         if (self.totalDuration + audioClip.duration > MAX_DURATION):
             return None
@@ -112,7 +114,7 @@ class ScreenshotScene:
     text = ""
     screenShotFile = ""
     commentId = ""
-    audioClip : AudioFileClip
+    audioClip: AudioFileClip
 
     def __init__(self, text, commentId, audioClip) -> None:
         self.text = text
@@ -122,7 +124,7 @@ class ScreenshotScene:
 
 class VoiceOver:
 
-    # To DO: 
+    # To DO:
     # (1) Implement Proper Engine Loop (done)
     # (2) Implement gender and voice changes as Class parameters
 
@@ -134,49 +136,48 @@ class VoiceOver:
     mp3_files: list[str] = []
     created_files: list[str] = []
 
-    bit_rate : int = engine.getProperty("rate")
-    #duration : float = 
+    bit_rate: int = engine.getProperty("rate")
+    # duration : float =
 
     def __init__(self, engine):
 
         self.engine = engine
         self.voiceoverDir = voiceoverDir
-        #self.engine.runAndWait()
+        # self.engine.runAndWait()
         self.engine.startLoop(True)
         self.filePath = filePath
         self.confirmed_files = confirmed_files
         self.created_files = created_files
         self.created_files = mp3_files
         self.bit_rate = bit_rate
-        self.duration = self.getDuration()
+        self.duration = self.get_durationV2(self.filePath)
 
     # Generates Voice over
+    # To Do:
     # Runs as a Loop in main.py
     # Generates Audio clip and stores generated clip data in Class Variables
 
     def create_voice_over(self, fileName, text) -> AudioFileClip:
         print(f"Creating Voicever :{ fileName}")
         self.filePath = f"{self.voiceoverDir}/{fileName}.mp3"
-        
+
         self.engine.save_to_file(text, self.filePath)
 
         if (self.locate_or_generate_mp3(self)):
 
             # Store File
-            self.created_files.append( self.filePath)
-      
-            #try :
+            self.created_files.append(self.filePath)
+
+            # try :
             #    # store Audio clip duration
             #    self.duration = MP3(self.filePath).info.length
-            #except Exception as err:
+            # except Exception as err:
             #    print (err)
-        
-            
+
             self.duration = self.get_durationV2(self.filePath)
 
             # Return audo clip
             return AudioFileClip(self.filePath)
-
 
     def locate_or_generate_mp3(self) -> bool:
         # A general purpose debugger for
@@ -192,57 +193,55 @@ class VoiceOver:
         # Checks if current file exists
         while not os.path.isfile(self.filePath):
             print(f"File not found at path: {self.filePath}, generating ")
-            
+
             self.engine.runAndWait()
-            #self.engine.startLoop(True)
+            # self.engine.startLoop(True)
             time.sleep(5)
 
-
-        # After file is generated         
+        # After file is generated
         if os.path.isfile(self.filePath):
-            print (f" Generated: {self.filePath}")
+            print(f" Generated: {self.filePath}")
 
-            #Breaks here
+            # Breaks here
 
-            #self.engine.runAndWait()
-            #print(f" Mp3 files debug: {self.mp3_files}")
-            return True    
+            # self.engine.runAndWait()
+            # print(f" Mp3 files debug: {self.mp3_files}")
+            return True
 
         if not self.filePath in self.confirmed_files:
             # append confirmed file path to list
             self.confirmed_files.append(self.filePath)
             return True
         return True
-    
+
     # Gets the Duration of the VoiceOver file and Saves it to VideoScript Class
-    def get_duration( mp3_file_path : str)-> float:
-            args=("ffprobe","-show_entries", "format=duration","-i",mp3_file_path)
-            popen = subprocess.Popen(args, stdout = subprocess.PIPE)
-            popen.wait()
-            output = popen.stdout.read()
+    def get_duration(mp3_file_path: str) -> float:
+        args = ("ffprobe", "-show_entries",
+                "format=duration", "-i", mp3_file_path)
+        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+        popen.wait()
+        output = popen.stdout.read()
 
-            # convert bytes to string
-            string = output.decode('utf-8')
+        # convert bytes to string
+        string = output.decode('utf-8')
 
-            if string != "":
-                print (f" duration debug 1: {string}") # for debug purposes only
-                
-                # find the start and end index of the float value
-                start_index = string.find('duration=') + len('duration=')
-                end_index = string.find('\n', start_index)
+        if string != "":
+            print(f" duration debug 1: {string}")  # for debug purposes only
 
-                # extract the float value
-                float_value = float(string[start_index:end_index])
-                #print (f" duration debug 2: {float_value}") # for debug purposes only
+            # find the start and end index of the float value
+            start_index = string.find('duration=') + len('duration=')
+            end_index = string.find('\n', start_index)
 
-                return float_value
+            # extract the float value
+            float_value = float(string[start_index:end_index])
+            # print (f" duration debug 2: {float_value}") # for debug purposes only
 
+            return float_value
 
-     # Gets the Duration of the VoiceOver file and Saves it to VideoScript Class
-    def get_durationV2( mp3_file_path : str)-> float:
-            try:
-                return ffmpeg.probe(mp3_file_path)['format']['duration']
-                
-            except ffmpeg.Error as error:
-                print(f"Error: {str(error)}")
+ # Gets the Duration of the VoiceOver file and Saves it to VideoScript Class
+    def get_durationV2(mp3_file_path: str) -> float:
+        try:
+            return ffmpeg.probe(mp3_file_path)['format']['duration']
 
+        except ffmpeg.Error as error:
+            print(f"Error: {str(error)}")
